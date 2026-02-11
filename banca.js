b92e430620d4a1ca47174338dddf81261b40d73b
Filippo <dimichelefilippo06@gmail.com>
2026-02-11 17:25:59 +0100

    implementazione fase 1 + commenti

--- banca.js
+++ banca.js
@@ -1,3 +1,30 @@
+
+// DATABASE BANCA
+
+let accounts = [
+    {
+    username:"utente1", //nome utente per il login
+    pin: "1212",        //pin personale dell'utente
+    saldo: 1000,        //saldo iniziale del conto
+    movimenti: [],      //storico delle operazioni
+},
+{
+    username: "utente2",
+    pin: "22222",
+    saldo: 100,
+    movimenti:[]
+},
+{
+    username: "admin", //utente speciale con permessi da amministratore 
+    pin: "12345",
+    saldo: 0,       //non ha saldo per coerenza al codice.
+    movimenti: []
+}
+];
+
+
+
+
 while (true) {
     let user = prompt("Inserisci username")
     let pin = prompt("Inserisci PIN")
